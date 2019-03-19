d898e408a9df22ffe67693bf8e1f07558828b3a5
chris pierre <chrispierrelouis.cpl@gmail.com>
2019-03-19 10:11:14 -0700

    setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -10,44 +10,45 @@ module.exports = (env) => {
     const CSSExtract = new ExtractTextPlugin('styles.css')
 
     console.log('env', env);
-return {
-    entry: "./src/app.js",
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [{
-            loader: "babel-loader",
-            test: /\.js$/,
-            exclude:/node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: CSSExtract.extract({
-                use:[
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true
+    return {
+        entry: "./src/app.js",
+        output: {
+            path: path.join(__dirname, "public", "dist"),
+            filename: "bundle.js"
+        },
+        module: {
+            rules: [{
+                loader: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
                         }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
-            })
-        }]
-    },
-    plugins: [
-        CSSExtract
-    ],
-    devtool: isProduction ? ' source-map': "inline-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback:true
-    }
-};
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? ' source-map' : "inline-source-map",
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true,
+            publicPath: "/dist/"
+        }
+    };
 };
